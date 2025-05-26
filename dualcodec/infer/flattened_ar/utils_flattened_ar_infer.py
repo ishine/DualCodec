1ce22f3634adb8ecbe9e4daa529a5b0244385d24
jiaqili3 <jiaqili3@link.cuhk.edu.cn>
2025-05-26 13:07:53 +0000

    instantiate flattened_ar model

---
+++ dualcodec/infer/flattened_ar/utils_flattened_ar_infer.py
@@ -0,0 +1,24 @@
+
+from dualcodec.infer.valle.utils_valle_infer import *
+def load_flattened_ar_model():
+    TTS_MODEL_CFG = {
+        "model": "flattened_ar",
+        "ckpt_path": "hf://amphion/dualcodec-tts/flattened_ar_dualcodec5x4096.safetensors",
+        # "ckpt_path": "dualcodec_tts_ckpts/dualcodec_flattened_ar_12hzv1.safetensors",
+        "cfg_path": "../conf_tts/model/flattened_ar/llama_1x32768.yaml",
+    }
+    model = (
+        instantiate_model(
+            model_cfg_path=TTS_MODEL_CFG["cfg_path"],
+        )
+        .half()
+        .eval()
+    )
+    ckpt_path = TTS_MODEL_CFG["ckpt_path"]
+    load_checkpoint(
+        model,
+        ckpt_path,
+        use_ema=False,
+        device=device,
+    )
+    return model
